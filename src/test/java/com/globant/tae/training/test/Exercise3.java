0edd21cefe1100be04fad145532e44673d511721
Roman Torres <roman.torres@globant.com>
2018-10-17 17:45:47 +0100

    Add documentation

--- src/test/java/com/globant/tae/training/test/Exercise3.java
+++ src/test/java/com/globant/tae/training/test/Exercise3.java
@@ -8,12 +8,22 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Exercise3 {
+    /**
+     * Property to store authorization token
+     */
     private String authorization;
 
+    /**
+     * Read authorization token from system properties
+     */
     @BeforeTest
     public void beforeTest() {
         authorization = System.getProperty("testAuth");
     }
+
+    /**
+     * Test that get resource using headers and parameters
+     */
     @Test
     public void searchWithParams() {
 
